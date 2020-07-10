75c0ffdf4a1e9f2dda96b695c5cbc43b28864da5
EnviroSolutions Sp. z o.o <office@envirosolutions.pl>
2020-07-10 15:32:58 +0200

    optymalizacja kodu

--- modules/utils.py
+++ modules/utils.py
@@ -4,6 +4,8 @@ import re, os
 import xml.etree.ElementTree as ET
 from .models import FormElement
 
+
+
 def showPopup(title, text, icon=QMessageBox.Information):
     msg = QMessageBox()
     msg.setWindowTitle(title)
@@ -16,29 +18,42 @@ def getNamespace(element):
     m = re.match(r'\{.*\}', element.tag)
     return m.group(0) if m else ''
 
-def createFormElementsRysunekAPP():
+def createFormElements(attribute):
     """Tworzy listę obiektów klasy 'FormElement'
     na podstawie pliku xsd"""
+
     xsd = os.path.join(os.path.dirname(__file__), 'validator', 'planowaniePrzestrzenne.xsd')
-    formElements = []
+
     ns = {'glowny': "http://www.w3.org/2001/XMLSchema",
           'app': "http://zagospodarowanieprzestrzenne.gov.pl/schemas/app/1.0",
           'gmd': "http://www.isotc211.org/2005/gmd",
           'gml': "http://www.opengis.net/gml/3.2",
           'gmlexr': "http://www.opengis.net/gml/3.3/exr"}
+    formElements = []
 
     tree = ET.parse(xsd)
     root = tree.getroot()
 
-    complexType = root.find("glowny:complexType[@name='RysunekAktuPlanowniaPrzestrzenegoType']", ns)
+    complexType = root.find("glowny:complexType[@name='" + attribute + "']", ns)
     sequence = complexType[0][0][0]  # sekwencja z listą pól
     for element in sequence:
         attrib = element.attrib
 
-        formElement = FormElement(
-            name=attrib['name'],
-            type=attrib['type']
-        )
+        try:
+            elementType = attrib['type']
+            formElement = FormElement(
+                name=attrib['name'],
+                type=elementType
+            )
+        except KeyError:
+            elementComplexType = element.find("glowny:complexType", ns)
+            elementAttrib = elementComplexType[0][0].attrib
+            elementType = elementAttrib['base']
+            formElement = FormElement(
+                name=attrib['name'],
+                type=elementType
+            )
+
         # na wypadek braku 'minOccurs'
         try:
             formElement.setMinOccurs(attrib['minOccurs'])
@@ -49,9 +64,10 @@ def createFormElementsRysunekAPP():
         formElement.setDocumentation(documentation.text)
 
         # zdefiniowany w app complextype
-        if attrib['type'][:4] == 'app:':
+        if elementType == 'app:IdentyfikatorPropertyType':
+        # if elementType[:4] == 'app:':
             formElement.markAsComplex()  # ustawia .isComplex = True
-            name = str(attrib['type']).replace('Property', '').split(':')[-1]
+            name = str(elementType).replace('Property', '').split(':')[-1]
 
             complexSequence = root.find("glowny:complexType[@name='%s']" % name, ns)[0]
             for complexElement in complexSequence:
@@ -68,93 +84,27 @@ def createFormElementsRysunekAPP():
 
     return formElements
 
+def createFormElementsRysunekAPP():
+    """Tworzy listę obiektów klasy 'FormElement'
+    na podstawie pliku xsd dla Rysunku APP"""
+
+    return createFormElements('RysunekAktuPlanowniaPrzestrzenegoType')
+
+
 def createFormElementsDokumentFormalny():
     #TODO lista rozwijalna dla atrybutu poziomHierarchii
     """Tworzy listę obiektów klasy 'FormElement'
-    na podstawie pliku xsd"""
-    xsd = os.path.join(os.path.dirname(__file__),'validator', 'planowaniePrzestrzenne.xsd')
-    formElements = []
-    ns = {'glowny': "http://www.w3.org/2001/XMLSchema",
-          'app': "http://zagospodarowanieprzestrzenne.gov.pl/schemas/app/1.0",
-          'gmd': "http://www.isotc211.org/2005/gmd",
-          'gml': "http://www.opengis.net/gml/3.2",
-          'gmlexr': "http://www.opengis.net/gml/3.3/exr"}
+    na podstawie pliku xsd dla Rysunku APP"""
 
-    tree = ET.parse(xsd)
-    root = tree.getroot()
-
-    complexType = root.find("glowny:complexType[@name='DokumentFormalnyType']", ns)
-    sequence = complexType[0][0][0]  # sekwencja z listą pól
-    for element in sequence:
-        attrib = element.attrib
-        try:
-            formElement = FormElement(
-                name=attrib['name'],
-                type=attrib['type']
-            )
-        except:
-
-            elementComplexType = element.find("glowny:complexType", ns)
-            elementAttrib = elementComplexType[0][0].attrib
-            formElement = FormElement(
-                name=attrib['name'],
-                type=elementAttrib['base']
-            )
-        # na wypadek braku 'minOccurs'
-        try:
-            formElement.setMinOccurs(attrib['minOccurs'])
-        except KeyError:
-            pass
-        # documentation
-        documentation = element.find("glowny:annotation", ns).find("glowny:documentation", ns)
-        formElement.setDocumentation(documentation.text)
-
-        formElements.append(formElement)
-
-    return formElements
+    return createFormElements('DokumentFormalnyType')
 
 def createFormElementsAktPlanowaniaPrzestrzennego():
     """Tworzy listę obiektów klasy 'FormElement'
-    na podstawie pliku xsd"""
-    xsd = os.path.join(os.path.dirname(__file__),'validator', 'planowaniePrzestrzenne.xsd')
-    formElements = []
-    ns = {'glowny': "http://www.w3.org/2001/XMLSchema",
-          'app': "http://zagospodarowanieprzestrzenne.gov.pl/schemas/app/1.0",
-          'gmd': "http://www.isotc211.org/2005/gmd",
-          'gml': "http://www.opengis.net/gml/3.2",
-          'gmlexr': "http://www.opengis.net/gml/3.3/exr"}
+    na podstawie pliku xsd dla Rysunku APP"""
 
-    tree = ET.parse(xsd)
-    root = tree.getroot()
+    return createFormElements('AktPlanowaniaPrzestrzennegoType')
 
-    complexType = root.find("glowny:complexType[@name='AktPlanowaniaPrzestrzennegoType']", ns)
-    sequence = complexType[0][0][0]  # sekwencja z listą pól
-    for element in sequence:
-        attrib = element.attrib
-        try:
-            formElement = FormElement(
-                name=attrib['name'],
-                type=attrib['type']
-            )
-        except:
-            elementComplexType = element.find("glowny:complexType", ns)
-            elementAttrib = elementComplexType[0][0].attrib
-            formElement = FormElement(
-                name=attrib['name'],
-                type=elementAttrib['base']
-            )
-        # na wypadek braku 'minOccurs'
-        try:
-            formElement.setMinOccurs(attrib['minOccurs'])
-        except KeyError:
-            pass
-        # documentation
-        documentation = element.find("glowny:annotation", ns).find("glowny:documentation", ns)
-        formElement.setDocumentation(documentation.text)
 
-        formElements.append(formElement)
-
-    return formElements
 
 def layout_widgets(layout):
     """iteracja widgetow wewnątrz layoutu"""

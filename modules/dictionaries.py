c484e25c453d818751050d974afcfea1ebb9adf5
EnviroSolutions Sp. z o.o <office@envirosolutions.pl>
2020-07-13 17:45:56 +0200

    aktualizacja list kodowych w związku z ich zmianą

--- modules/dictionaries.py
+++ modules/dictionaries.py
@@ -37,33 +37,27 @@ nilReasons = {
         "unknown": "nieznany",
         "withheld": "wstrzymany"}
 ukladyOdniesieniaPrzestrzennego = {
-    "PL-1992" : "http://www.opengis.net/def/crs/EPSG/0/2180",
-    "PL-2000(5)" : "http://www.opengis.net/def/crs/EPSG/0/2176",
-    "PL-2000(6)" : "http://www.opengis.net/def/crs/EPSG/0/2177",
-    "PL-2000(7)" : "http://www.opengis.net/def/crs/EPSG/0/2178",
-    "PL-2000(8)" : "http://www.opengis.net/def/crs/EPSG/0/2179"
+    "PL-1992": "http://www.opengis.net/def/crs/EPSG/0/2180",
+    "PL-2000(5)": "http://www.opengis.net/def/crs/EPSG/0/2176",
+    "PL-2000(6)": "http://www.opengis.net/def/crs/EPSG/0/2177",
+    "PL-2000(7)": "http://www.opengis.net/def/crs/EPSG/0/2178",
+    "PL-2000(8)": "http://www.opengis.net/def/crs/EPSG/0/2179"
 }
 typyPlanu = {
-    "plan zagospodarowania przestrzennego województwa" : "planZagospodarowaniaPrzestrzennegoWojewodztwa",
-    "pzp miejskiego obszaru funkcjonalnego ośrodka wojewódzkiego" : "planZagospodarowaniaPrzestrzennegoMiejskiegoObszaruFunkcjonalnegoOśrodkaWojewódzkiego",
-    "rsuikzp związku metropolitalnego" : "ramoweStudiumUwarunkowanIKierunkowZagospodarowaniaPrzestrzennegoZwiazkuMetropolitalnego",
-    "suikzp gminy" : "studiumUwarunkowanIKierunkowZagospodarowaniaPrzestrzennegoGminy",
-    "mpzp" : "miejscowyPlanZagospodarowaniaPrzestrzennego",
-    "miejscowy plan odbudowy" : "miejscowyPlanOdbudowy",
-    "miejscowy plan rewitalizacji" : "miejscowyPlanRewitalizacji"
+    "plan zagospodarowania przestrzennego województwa": "planZagospodarowaniaPrzestrzennegoWojewodztwa",
+    "studium uwarunkowań i kierunków zagospodarowania przestrzennego gminy": "studiumUwarunkowanIKierunkowZagospodarowaniaPrzestrzennegoGminy",
+    "miejscowy plan zagospodarowania przestrzennego": "miejscowyPlanZagospodarowaniaPrzestrzennego",
+    "miejscowy plan odbudowy": "miejscowyPlanOdbudowy",
+    "miejscowy plan rewitalizacji": "miejscowyPlanRewitalizacji"
 }
 
 poziomyHierarchii = {
-    "Koncepcja przestrzennego zagospodarowania kraju – krajowy" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/national",
-    "Plan zagospodarowania przestrzennego województwa - regionalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/regional",
-	"Plan zagospodarowania przestrzennego miejskiego obszaru funkcjonalnego ośrodka wojewódzkiego – subregionalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraRegional",
-	"Ramowe studium uwarunkowań i kierunków zagospodarowania przestrzennego związku metropolitalnego - ponadlokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/supraLocal",
-	"Miejscowy plan zagospodarowania przestrzennego obejmujący obszar całej gminy - lokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/local",
-	"Studium uwarunkowań i kierunków zagospodarowania przestrzennego - lokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/local",
-	"Miejscowy plan zagospodarowania przestrzennego obejmujący obszar części gminy - sublokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal",
-	"Miejscowy plan odbudowy obiektów budowlanych - sublokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal",
-	"Miejscowy plan rewitalizacji - sublokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal",
-	"Uchwała w przedmiocie wyznaczenia obszaru zabudowanego – sublokalny" : "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal"
+    "Plan zagospodarowania przestrzennego województwa - regionalny": "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/regional",
+    "Studium uwarunkowań i kierunków zagospodarowania przestrzennego - lokalny": "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/local",
+    "Miejscowy plan zagospodarowania przestrzennego obejmujący obszar całej gminy - lokalny": "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/local",
+    "Miejscowy plan zagospodarowania przestrzennego obejmujący obszar części gminy - sublokalny": "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal",
+    "Miejscowy plan odbudowy - sublokalny": "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal",
+    "Miejscowy plan rewitalizacji - sublokalny": "http://inspire.ec.europa.eu/codelist/LevelOfSpatialPlanValue/infraLocal"
 }
 
 statusListaKodowa = {

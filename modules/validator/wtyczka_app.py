eba4aa9ef7817211f05823d5745138d6983d28ea
Michal Wloga <michal.wloga@envirosolutions.pl>
2020-07-21 09:34:29 +0200

    oprogramowanie okna walidacji

--- modules/validator/wtyczka_app.py
+++ modules/validator/wtyczka_app.py
@@ -13,6 +13,7 @@ class ValidatorModule(BaseModule):
     def __init__(self, iface):
         self.iface = iface
         self.dataValidator = None   # inicjacja w głównym skrypcie wtyczki
+        self.metadataValidator = None   # inicjacja w głównym skrypcie wtyczki
         # region okno moduł validator
         self.walidacjaDialog = WalidacjaDialog()
         # endregion
@@ -20,7 +21,6 @@ class ValidatorModule(BaseModule):
         # region eventy moduł validator
         #self.walidacjaDialog.prev_btn.clicked.connect(self.walidacjaDialog_prev_btn_clicked)
 
-        self.walidacjaDialog.close_btn.setEnabled(False)
         self.walidacjaDialog.validate_btn.clicked.connect(self.walidacjaDialog_validate_btn_clicked)
         self.walidacjaDialog.validateGML_radioButton.toggled.connect(self.gml_checkBoxChangedAction)
         self.walidacjaDialog.validateXML_radioButton.toggled.connect(self.xml_checkBoxChangedAction)
@@ -36,22 +36,20 @@ class ValidatorModule(BaseModule):
         self.openNewDialog(self.listaOkienek.pop())
 
     def walidacjaDialog_validate_btn_clicked(self):
-        if self.dataValidator:  # walidator gotowy do dzialania
-            validationResult = self.dataValidator.validateXml()
-            if validationResult[0]:  # poprawnie zwalidowano
-                print('OK')
-                self.showPopupValid()
-                self.walidacjaDialog.close_btn.setEnabled(True)
-                self.iface.messageBar().pushSuccess("Sukces:",
-                                                    "Pomyślnie zwalidowano plik. Nie wykryto błędów.")
-            else:
-                # print(validationResult[1])
-                self.showPopupInvalid(validationResult[1])
-                self.iface.messageBar().pushCritical("Błąd walidacji:",
-                                                    "Wykryto błędy walidacji.")
-        else: # walidator niegotowy do dzialania - nadal wczytuje XSD
-            self.iface.messageBar().pushWarning("Ostrzeżenie:", "Schemat danych nie został jeszcze zaimportowany, spróbuj ponownie za chwilę.")
 
+        if self.walidacjaDialog.validateGML_radioButton.isChecked():  # wybrnao walidację GML
+            path = self.walidacjaDialog.chooseGML_widget.filePath()
+            if path:  # jest wybrany plik z danymi
+                self.__validateFile(path=path, validator=self.dataValidator)
+            else:   # brak pliku z danymi
+                self.iface.messageBar().pushWarning("Ostrzeżenie:", "Nie wksazano pliku z danymi.")
+
+        elif self.walidacjaDialog.validateXML_radioButton.isChecked():    # wybrano walidację XML
+            path = self.walidacjaDialog.chooseXML_widget.filePath()
+            if path:  # jest wybrany plik z metadanymi
+                self.__validateFile(path=path, validator=self.metadataValidator)
+            else:  # brak pliku z metadanymi
+                self.iface.messageBar().pushWarning("Ostrzeżenie:", "Nie wksazano pliku z metadanymi.")
 
     def xml_checkBoxChangedAction(self, state):
         self.walidacjaDialog.chooseXML_widget.setEnabled(state)
@@ -62,13 +60,26 @@ class ValidatorModule(BaseModule):
     # endregion
 
     """Helper methods"""
+    def __validateFile(self, path, validator):
+        """walidacja pliku z danymi lub metadanymi"""
+        if validator:  # walidator gotowy do dzialania
+            validationResult = self.dataValidator.validateXml(xmlPath=path)
+            if validationResult[0]:  # poprawnie zwalidowano
+                self.iface.messageBar().pushSuccess("Sukces:", "Pomyślnie zwalidowano plik. Nie wykryto błędów.")
+                self.showPopupValid()
+            else:   # błędy walidacji
+                self.iface.messageBar().pushCritical("Błąd walidacji:", "Wykryto błędy walidacji.")
+                self.showPopupInvalid(validationResult[1])
+        else:  # walidator niegotowy do dzialania - nadal wczytuje XSD
+            self.iface.messageBar().pushWarning("Ostrzeżenie:",
+                                                "Schemat danych nie został jeszcze zaimportowany, spróbuj ponownie za chwilę.")
 
     """Popup windows"""
     def showPopupExport(self):
         showPopup("Wyeksportuj plik z błędami", "Poprawnie wyeksportowano plik z błędami.")
 
     def showPopupValid(self):
-        showPopup("Waliduj pliki", "Poprawnie zwalidowano wszystkie wgrane pliki.")
+        showPopup("Waliduj pliki", "Poprawnie zwalidowano plik.")
 
     def showPopupInvalid(self, bledy):
         showPopup("Błąd walidacji", "Wystąpiły błędy walidacji pliku:\n\n" + bledy)

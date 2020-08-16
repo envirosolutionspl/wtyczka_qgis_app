e6c0fec4578cf7a3d305e546886450541c1c3cf2
Michal Wloga <michal.wloga@envirosolutions.pl>
2020-08-16 14:58:29 +0200

    praca z APP 3/6: sprawdzanie czy warstwa leży całkowicie w Polsce

--- modules/app/app_utils.py
+++ modules/app/app_utils.py
@@ -1,15 +1,16 @@
-from qgis.core import QgsProject, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsRectangle
+from . import poland
+from qgis.core import QgsProject, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsGeometry, QgsRectangle
 
 
 def isLayerInPoland(obrysLayer):
     """sprawdza czy geometria obrysu jest poprawna"""
-    crs4326 = QgsCoordinateReferenceSystem(4326)  # WGS84
+    # definicja transformacji układu
     layerCrs = obrysLayer.sourceCrs()  # z warstwy
-    transform = QgsCoordinateTransform(
-        layerCrs, crs4326, QgsProject.instance())
+    crs4326 = QgsCoordinateReferenceSystem(4326)  # WGS84
+    transform = QgsCoordinateTransform(layerCrs, crs4326, QgsProject.instance())
 
-    layerExtent = obrysLayer.sourceExtent()
-    layerExtent4326 = transform.transform(layerExtent)
-    polandExtent4326 = QgsRectangle(
-        14.0745211117, 49.0273953314, 24.0299857927, 54.8515359564)
-    return polandExtent4326.intersects(layerExtent4326)
+    feat = next(obrysLayer.getFeatures())
+    geom_obrysLayer = feat.geometry()
+    geom_obrysLayer.transform(transform)
+    geom_poland = QgsGeometry.fromWkt(poland.wkt)
+    return geom_obrysLayer.within(geom_poland)
